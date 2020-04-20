048e0318626b4b20b466d9fcfa7a9dc88da21f30
hposborn <hugh.osborn@space.unibe.ch>
2020-04-20 15:45:41 -0400

    theano configs

--- __init__.py
+++ __init__.py
@@ -1,14 +1,9 @@
-import theano
-#This forces it to use CPUs:
-theano.config.floatX = 'float32' 
-theano.config.set_device = 'cpu' 
-theano.config.force_device = 'True' 
+import os
+os.environ['THEANO_FLAGS'] = "device=cpu"
+McmcTools_path = os.path.dirname(os.path.abspath( __file__ ))
 
 from . import tools
 from . import MonoSearch
 from . import MonoFit
 from .stellar import starpars
-
-import os
-McmcTools_path = os.path.dirname(os.path.abspath( __file__ ))
 
