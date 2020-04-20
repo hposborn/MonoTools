d5af307c9dda2457e7c6b3d6c7ddc2063fb4507d
hposborn <hugh.osborn@space.unibe.ch>
2020-04-20 15:42:09 -0400

    theano configs

--- __init__.py
+++ __init__.py
@@ -5,3 +5,9 @@ from .stellar import starpars
 
 import os
 McmcTools_path = os.path.dirname(os.path.abspath( __file__ ))
+
+import theano
+#This forces it to use CPUs:
+theano.config.floatX = 'float32' 
+theano.config.set_device = 'cpu' 
+theano.config.force_device = 'True' 
