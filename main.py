c46632578e3365421db66c6127da19b94070318d
hposborn <hugh.osborn@space.unibe.ch>
2020-06-22 21:54:14 -0400

    bug fixing

--- main.py
+++ main.py
@@ -90,8 +90,6 @@ variable_llk_thresh=float(sys.argv[9]) if len(sys.argv)>9 else 5
 file_loc=sys.argv[10] if len(sys.argv)>10 else None
 plot=bool(sys.argv[11]) if len(sys.argv)>11 else True
 do_fit=bool(sys.argv[12]) if len(sys.argv)>12 else True
-re_vet=bool(sys.argv[13]) if len(sys.argv)>13 else True
-re_fit=bool(sys.argv[14]) if len(sys.argv)>14 else True
 
 #Only running if we haven't already created a report:
 if not os.path.exists(os.path.join(x_file_loc,id_dic[mission]+str(ID).zfill(11)+"_report.pdf")) or overwrite is not None:
@@ -102,7 +100,7 @@ if not os.path.exists(os.path.join(x_file_loc,id_dic[mission]+str(ID).zfill(11)+
                                     tcen=tcen,tdur=tdur,overwrite=overwrite,do_search=do_search,
                                     useL2=useL2,PL_ror_thresh=PL_ror_thresh,
                                     variable_llk_thresh=variable_llk_thresh,file_loc=file_loc,
-                                    plot=plot,do_fit=do_fit,re_vet=re_vet,re_fit=re_fit, use_GP=False)
+                                    plot=plot,do_fit=do_fit, use_GP=False)
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print(e, exc_type, os.path.split(exc_tb.tb_frame.f_code.co_filename)[1], exc_tb.tb_lineno, ID,mission,"problem")
