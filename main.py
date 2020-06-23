2e34022ae5999fcec25f3821914db67dc8e4e0cb
hposborn <hugh.osborn@space.unibe.ch>
2020-06-23 18:31:54 -0400

    wtf bugs?2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ mpl.use('Agg')
 import os
 os.nice(4)
 
-
+log=False
 ##############################
 #  FORCING LOGGING TO FILE:  #
 ##############################
@@ -30,52 +30,52 @@ mission=sys.argv[2]
 x_file_loc=MonoData_savepath+'/'+id_dic[mission]+str(ID).zfill(11)
 if not os.path.isdir(x_file_loc):
     os.mkdir(x_file_loc)
+if log:
+    #Functions needed to make this stream logging work:
+    sys.stdout.isatty = lambda: False
 
-#Functions needed to make this stream logging work:
-sys.stdout.isatty = lambda: False
+    class StreamToLogger(object):
+        """
+        Fake file-like stream object that redirects writes to a logger instance.
+        """
+        def __init__(self, logger, log_level=logging.INFO):
+            self.logger = logger
+            self.log_level = log_level
+            self.linebuf = ''
 
-class StreamToLogger(object):
-    """
-    Fake file-like stream object that redirects writes to a logger instance.
-    """
-    def __init__(self, logger, log_level=logging.INFO):
-        self.logger = logger
-        self.log_level = log_level
-        self.linebuf = ''
+        def write(self, buf):
+            temp_linebuf = self.linebuf + buf
+            self.linebuf = ''
+            for line in temp_linebuf.splitlines(True):
+                # From the io.TextIOWrapper docs:
+                #   On output, if newline is None, any '\n' characters written
+                #   are translated to the system default line separator.
+                # By default sys.stdout.write() expects '\n' newlines and then
+                # translates them so this is still cross platform.
+                if line[-1] == '\n':
+                    self.logger.log(self.log_level, line.rstrip())
+                else:
+                    self.linebuf += line
 
-    def write(self, buf):
-        temp_linebuf = self.linebuf + buf
-        self.linebuf = ''
-        for line in temp_linebuf.splitlines(True):
-            # From the io.TextIOWrapper docs:
-            #   On output, if newline is None, any '\n' characters written
-            #   are translated to the system default line separator.
-            # By default sys.stdout.write() expects '\n' newlines and then
-            # translates them so this is still cross platform.
-            if line[-1] == '\n':
-                self.logger.log(self.log_level, line.rstrip())
-            else:
-                self.linebuf += line
+        def flush(self):
+            if self.linebuf != '':
+                self.logger.log(self.log_level, self.linebuf.rstrip())
+            self.linebuf = ''
 
-    def flush(self):
-        if self.linebuf != '':
-            self.logger.log(self.log_level, self.linebuf.rstrip())
-        self.linebuf = ''
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+        filename=os.path.join(x_file_loc,id_dic[mission]+str(ID).zfill(11)+"_sysout.log"),
+        filemode='a'
+    )
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
-    filename=os.path.join(x_file_loc,id_dic[mission]+str(ID).zfill(11)+"_sysout.log"),
-    filemode='a'
-)
+    stdout_logger = logging.getLogger('STDOUT')
+    sl = StreamToLogger(stdout_logger, logging.INFO)
+    sys.stdout = sl
 
-stdout_logger = logging.getLogger('STDOUT')
-sl = StreamToLogger(stdout_logger, logging.INFO)
-sys.stdout = sl
-
-stderr_logger = logging.getLogger('STDERR')
-sl = StreamToLogger(stderr_logger, logging.ERROR)
-sys.stderr = sl
+    stderr_logger = logging.getLogger('STDERR')
+    sl = StreamToLogger(stderr_logger, logging.ERROR)
+    sys.stderr = sl
 
 os.environ["THEANO_FLAGS"] = "device=cpu,floatX=float32,cxxflags = -fbracket-depth=1024,base_compiledir="+x_file_loc
 
