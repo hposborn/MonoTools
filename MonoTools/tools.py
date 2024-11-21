f9b2d501036f03757523220badeeb496b262c1e8
hposborn <hugh.osborn@space.unibe.ch>
2024-11-21 13:17:17 +0100

    bug fixing binning function

--- MonoTools/tools.py
+++ MonoTools/tools.py
@@ -1426,7 +1426,7 @@ def bin_light_curve(time, flux, flux_err= None,
     else:
         avbinsize=np.nanmedian(np.diff(new_time_bins))
         #Making bin divisions half way between each defined x point here
-        bins=np.hstack((new_time_bins[0]-avbinsize,0.5*(new_time_bins[:-1]+new_time_bins[1:]),new_time_bins[-1]+0.5*avbinsize,new_time_bins[-1]+1.5*avbinsize))
+        bins=np.hstack((new_time_bins[0]-0.5*avbinsize,0.5*(new_time_bins[:-1]+new_time_bins[1:]),new_time_bins[-1]+0.5*avbinsize,new_time_bins[-1]+1.5*avbinsize))
 
     # Digitize the time array into the created bins
     bin_indices = np.digitize(time, bins) - 1
